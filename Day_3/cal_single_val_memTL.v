d6530a40902d87c81ba080a89a224f121665b11b
Shreyas-borse <49656689+Shreyas-borse@users.noreply.github.com>
2025-07-26 12:53:04 -0700

    26to31-Calculator-Valid-Single Value Memory Lab

--- Day_3/cal_single_val_memTL.v
+++ Day_3/cal_single_val_memTL.v
@@ -1,37 +1,51 @@
-\m5_TLV_version 1d: tl-x.org
-\m5
-   
-   // ============================================
-   // Welcome, new visitors! Try the "Learn" menu.
-   // ============================================
-   
-   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\m4_TLV_version 1d: tl-x.org
 \SV
-   // Macro providing required top-level module definition, random
-   // stimulus support, and Verilator config.
-   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
-\TLV
-   $valid_or_reset = $valid | $reset;
+   // This code can be found in: https://github.com/stevehoover/RISC-V_MYTH_Workshop
+   
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/RISC-V_MYTH_Workshop/ecba3769fff373ef6b8f66b3347e8940c859792d/tlv_lib/calculator_shell_lib.tlv'])
 
+\SV
+   m4_makerchip_module   // (Expanded in Nav-TLV pane.)
+
+\TLV
    |calc
+      @0
+         $reset = *reset;
+      @1
+         $val1[31:0] = >>2$out[31:0];
+         $val2[31:0] = $rand2[3:0];
+         
+         $valid = $reset ? 1'b0 : >>1$valid + 1'b1;
+         $valid_or_reset = $valid || $reset;
+         
       ?$valid_or_reset
          @1
-            $val1[31:0] = >>2$out;
-            $val2[31:0] = $rand2[3:0];
-
             $sum = $val1 + $val2 ;
             $diff = $val1 - $val2 ;
             $prod = $val1 * $val2 ;
             $qout = $val1 / $val2 ;
 
          @2
-            $op[2:0] == 3'b000 ? $out1 = $sum : ($op[2:0] = 3'b001) ? $out1 = $diff : ($op[2:0] = 3'b010) ? $out1 = $prod : ($op[2:0] = 3'b011) ? $out1 = $qout : ($op[2:0] = 3'b100) ? $out = >>2$mem  : ($op[2:0] = 3'b101) ? $mem[31:0] = >>2$out[31:0] : $mem[31:0] = >>2$mem[31:0];
+            $out1[31:0] = ($op[2:0] == 3'b000) ? $sum : ($op[2:0] = 3'b001) ? $diff : ($op[2:0] = 3'b010) ?  $prod : ($op[2:0] = 3'b011) ?  $qout : ($op[2:0] = 3'b100) ?  >>2$mem : >>2$out ;
+            $out[31:0] = $valid_or_reset ? 32'b0 : $out1[31:0];
+            $mem[31:0] = $reset ? 32'b0 : ($op[2:0] == 3'b101) ? $val1[31:0] : >>2$mem[31:0];
+            
+      // Macro instantiations for calculator visualization(disabled by default).
+      // Uncomment to enable visualisation, and also,
+      // NOTE: If visualization is enabled, $op must be defined to the proper width using the expression below.
+      //       (Any signals other than $rand1, $rand2 that are not explicitly assigned will result in strange errors.)
+      //       You can, however, safely use these specific random signals as described in the videos:
+      //  o $rand1[3:0]
+      //  o $rand2[3:0]
+      //  o $op[x:0]
+      
+   //m4+cal_viz(@3) // Arg: Pipeline stage represented by viz, should be atleast equal to last stage of CALCULATOR logic.
 
-            $out = $valid_or_reset ? 32'b0 : $out1;
-            $mem[31:0] = $valid_or_reset ? 32'b0 : >>2$mem;
-
-   // Assert these to end simulation (before the cycle limit).
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
    *passed = *cyc_cnt > 40;
    *failed = 1'b0;
+   
+
 \SV
    endmodule
